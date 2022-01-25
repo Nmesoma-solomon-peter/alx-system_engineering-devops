64b7dfcf03a5b02674168348ad5e683bbf27e1aa
nmesoma <nmesomasolo2809@gmail.com>
2022-01-25 19:04:51 +0100

    task 7-puppet added

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# puppet manifest that installs nginx
+# prints 'hello world!' on default page
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/mrbridge-mrbridge permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
