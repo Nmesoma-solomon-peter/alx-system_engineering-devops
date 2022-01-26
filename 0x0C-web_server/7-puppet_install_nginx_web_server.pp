4e41ca8cf72a3219c41fc2ba959456c73a78f2ca
nmesoma <nmesomasolo2809@gmail.com>
2022-01-26 01:34:04 +0100

    added and modified task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,7 +9,7 @@ file_line { 'redirect':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://github.com/mrbridge-mrbridge permanent;',
+  line   => 'rewrite ^/redirect_me https://github.com/Nmesoma-solomon-peter permanent;',
 }
 
 file { '/var/www/html/index.html':
